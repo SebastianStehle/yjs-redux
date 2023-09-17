3f0ad8d1b2a112f0a9dfba9e2a8986efff2acc4c
Sebastian <sebastian@squidex.io>
2023-09-17 19:20:56 +0200

    Another test.

--- src/binder.ts
+++ src/binder.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-
 import * as Y from 'yjs';
 import { Middleware, Reducer } from 'redux';
 import { initToYJS, syncToYJS } from './sync-to-yjs';
